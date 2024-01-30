a58ebf61dc417a16638635c7fa0cf707123aa682
Ali8x8 <ali.raza@8x8.com>
2024-01-30 19:11:13 +0500

    Commit After Update Pom.xml and now Script is running successfully.

---
+++ src/test/java/Runner/Runner.java
@@ -0,0 +1,24 @@
+package Runner;
+
+import org.junit.runner.RunWith;
+import org.testng.annotations.Test;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+@RunWith(Cucumber.class)
+
+@CucumberOptions(features="src/test/java/formfeature/form1.feature",
+/*@CucumberOptions(features={"src\\test\\java\\FEATURE\\businessPhone.feature",
+"src\\test\\java\\FEATURE\\videoconferencing.feature","src\\test\\java\\FEATURE\\teamchat.feature",
+"src\\test\\java\\FEATURE\\integration.feature","src\\test\\java\\FEATURE\\contactcenter.feature",
+"src\\test\\java\\FEATURE\\api.feature","src\\test\\java\\FEATURE\\download.feature"},*/
+glue="formDefinition",
+dryRun = false,
+monochrome = true,
+//plugin = {"pretty" , "html: test-output"},
+plugin = {"pretty", "json:target/cucumber-reports/cucumber.json",
+		"html:target/cucumber-reports/cucumber.html"}
+		)
+public class Runner {
+
+}
