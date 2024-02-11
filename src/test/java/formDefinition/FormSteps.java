ddc56173bb33ec93b0d68f55f55db4adde5e5aa1
Ali8x8 <ali.raza@8x8.com>
2024-02-11 21:13:57 +0500

    junit jupiter plugins added

--- src/test/java/formDefinition/FormSteps.java
+++ src/test/java/formDefinition/FormSteps.java
@@ -2,11 +2,11 @@ package formDefinition;
 
 import java.io.IOException;
 
+import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.testng.annotations.Test;
 
 import Req.REQPOM;
 import Utilities.BaseClass;
@@ -18,7 +18,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FormSteps extends BaseClass {
 
-	@org.junit.Test
+	@Test
 	@Given("Launch the browser")
 	public void launch_the_browser() {
 
